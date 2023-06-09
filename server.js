5a889d999f935d12bad9b8463bce10cf965ca0d9
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-09 08:49:33 +0300

    Add views for index.html, add routes for root.js, add public folder with css file

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 4000
 
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', require("./routes/root"))
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
