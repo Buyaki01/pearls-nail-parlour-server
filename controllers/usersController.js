933eaeaa681ad0a72472ccdfa6d55d090785615e
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-10 09:41:44 +0300

    Add to getAllusers and createNewUser methods

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,14 +7,44 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req,res) => {
-  
+  const users = await User.find().select('-password').lean()
+  if (!users) {
+    return res.status(400).json({ message: 'No users found' })
+  }
+  res.json(users)
 })
 
 // @describe Create new user
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req,res) => {
-  
+  const { username, password, roles } = req.body
+
+  //Confirm Data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+
+  //Check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec()
+
+  if (duplicate) {
+    return res.status(409).json({ message: 'Duplicate username'})
+  }
+
+  //Hash Password
+  const hashedPwd = await bcrypt.hash(password, 10) //Salt Rounds
+
+  const userObject = { username, "password": hashedPwd, roles }
+
+  //Create and store new user
+  const user = await User.create(userObject)
+
+  if (user) { //Created
+    res.status(201).json({ message: `New user ${username} created` })
+  } else {
+    res.status(400).json({ message: 'Invalid user data received' })
+  }
 })
 
 // @describe Update user
