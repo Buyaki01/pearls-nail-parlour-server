317295e0d6266cf7f6db8ad66f2f1190e8a91993
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-10 10:16:08 +0300

    Add to updateUser method

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req,res) => {
-  const users = await User.find().select('-password').lean()
+  const users = await User.find().select('-password').lean() //we don't need moongose methods to be returned with it
   if (!users) {
     return res.status(400).json({ message: 'No users found' })
   }
@@ -51,7 +51,37 @@ const createNewUser = asyncHandler(async (req,res) => {
 // @route PUT /users
 // @access Private
 const updateUser = asyncHandler(async (req,res) => {
-  
+  const { id, username, roles, active, password } = req.body
+
+  //Confirm Data
+  if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+
+  const user = await User.findById(id).exec() //Using .exec() as we are assigning a parameter and a promise is needed
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' })
+  }
+
+  //Check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec()
+  //Allow updates to the original user not current user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: 'Duplicate Username' })
+  }
+
+  user.username = username
+  user.roles = roles
+  user.active = active
+
+  if (password) {
+    //Hash password
+    user.password = await bcrypt.hash(password, 10) //Salt Rounds
+  }
+
+  const updatedUser = await user.save()
+
+  res.json({ message: `${updatedUser.username} updated`})
 })
 
 // @describe Delete user
