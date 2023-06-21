0d013a9036d9860cac8bd48e6a19850ac4c4fc26
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-21 15:00:34 +0300

    Add logic for the refresh method in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,36 @@ const login = asyncHandler(async (req, res) => {
 })
 
 const refresh = (req, res) => {
+  const cookies = req.cookies
 
+  if (!cookies?.jwt) return res.status(401).json({ message: 'Unauthorized' })
+
+  const refreshToken = cookies.jwt
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: 'Forbidden' })
+
+      const foundUser = await User.findOne({ username: decoded.username})
+
+      if (!foundUser) return res.status(401).json({ message: 'Unauthorized'})
+
+      const accessToken = jwt.sign(
+        {
+          "UserInfo": {
+            "username": foundUser.username,
+            "roles": foundUser.roles
+          }
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '10s' }
+      )
+
+      res.json({ accessToken })
+    })
+  )
 }
 
 const logout = (req,res) => {
