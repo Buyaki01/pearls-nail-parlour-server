bc4fe4bb846c5e6126718880f3c04eb393ff2279
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-21 15:10:23 +0300

    Add logic for the logout  method in auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,7 +82,10 @@ const refresh = (req, res) => {
 }
 
 const logout = (req,res) => {
-
+  const cookies = req.cookies
+  if (!cookies?.jwt) return res.sendStatus(204) //No content
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
+  res.json({ message: 'Cookie cleared' })
 }
 
 module.exports = { login, refresh, logout }
