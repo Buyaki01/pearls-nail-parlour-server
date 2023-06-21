581c34a2069c3841f566e4967f225f4f2f1ab46c
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-21 14:09:32 +0300

    Add logic for the login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,48 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 
 const login = asyncHandler(async (req, res) => {
-   
+  const { username, password } = req.body
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+
+  const foundUser = await User.findOne({ username }).exec()
+
+  if (!foundUser || !foundUser.active) {
+    return res.status(401).json({ message: 'Unauthorized' })
+  }
+
+  const match = await bcrypt.compare(password, foundUser.password)
+
+  if (!match) return res.status(401).json({ message: 'Unauthorized' })
+
+  const accessToken = jwt.sign(
+    {
+      "UserInfo": {
+        "username": foundUser.username,
+        "roles": foundUser.roles
+      }
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: '10s' }
+  )
+
+  const refreshToken = jwt.sign(
+    { "username": foundUser.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: '7d' }
+  )
+
+  //Create secure cookie with refresh token
+  res.cookie('jwt', refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None', //cross-site cookie
+    maxAge: 7 * 24 * 60 * 60 * 1000 
+  })
+
+  res.json({ accessToken}) //Send accessToken containing username and roles
 })
 
 const refresh = (req, res) => {
