5243582754e676baa490745fdbc4efc3bdf0f97d
Buyaki01 <rittahbuyaki@gmail.com>
2023-06-26 13:05:22 +0300

    Add isError: true to errorHandler middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
 
   const status = res.statusCode ? res.statusCode : 500
   res.status(status)
-  res.json({ message: err.message })
+  res.json({ message: err.message, isError: true })
 }
 
 module.exports = errorHandler
